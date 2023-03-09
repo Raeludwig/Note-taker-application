5c81ea16995cbe46eae24e241c7f6abf79e45731
Raeludwig <racheleludwig@gmail.com>
2023-03-08 22:54:15 -0700

    still trying to fix saving issues

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,5 +13,26 @@ app.use(express.static('public'));
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname), "./public/index.html"))
+});
+
+app.get('/api/notes', (req, res) => {
+    const savedNotes = db;
+    res.json(savedNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const savedNotes = db;
+    const newNote = req.body;
+    savedNotes.push(newNote);
+    console.log(savedNotes);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.parse(savedNotes));
+    res.status(200).json(savedNotes);
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
